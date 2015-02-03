3ef2ce411eaedfe4ae6e13d3e58af84d6849696f
Wouter Bulten <wouterbulten@mai-ai.org>
2015-02-03 18:00:31 +0100

    Added RSSI to nodes

--- network/nodes.py
+++ network/nodes.py
@@ -1,10 +1,24 @@
 """
 @author Wouter Bulten
 """
+import math
 
 class Node:
 
-	def __init__(self, x, y):
+	def __init__(self, x, y, txPower = -59, n = 2):
 
-		self._x = x;
-		self._y = y;
+		self._x = x
+		self._y = y
+		self._txPower = txPower
+		self._n = 2 #Signal propagation constant
+
+	def getSignalStrengthAtLocation(self, x, y):
+		return self.RSSI(self.getDistance(x,y))
+
+	def getDistance(self, x, y):
+		"""Get the distance between this node and a specific point"""
+		return math.sqrt(math.pow((x - self._x),2) + math.pow((y - self._y),2))
+
+	def RSSI(self, dist):
+		"""Get the RSSI strength at a given distance"""
+		return -(10 * self._n) *  math.log10(dist) + self._txPower
--- simulation.py
+++ simulation.py
@@ -4,12 +4,16 @@ import environment.world as world
 config = {
 	
 	'nNodes':	5,
-	'xMax':		100,
-	'yMax':		100,
+	'xMax':		50,
+	'yMax':		50,
 }
 
+# Create a new world
 world = world.World(config['xMax'], config['yMax'])
 
+# Instantiate nodes with a random position
 nodes = [nodes.Node(*world.getRandomPosition()) for x in range(0, config['nNodes'])]
 
-print nodes
+print nodes[0]._x
+print nodes[0]._y
+print [node.getSignalStrengthAtLocation(50,50) for node in nodes]
